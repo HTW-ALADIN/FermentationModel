f5936f3ca2e0ec9032691af300544565ff028d4b
Paul Christ <paul.l.christ@web.de>
2025-12-19 07:10:22 +0100

    revert: build-folder

--- build/lib/InputAdapter/base_input_adapter.py
+++
@@ -1,9 +0,0 @@
-from Util.base_io_adapter import IOAdapter
-from abc import abstractmethod
-import pandas as pd
-import json
-
-class InputAdapter(IOAdapter):
-    @abstractmethod
-    def transform_data(self, data: json) -> pd.DataFrame:
-        return
--- build/lib/OutputAdapter/df_adapter.py
+++
@@ -1,7 +0,0 @@
-from .base_output_adapter import OutputAdapter
-import pandas as pd
-
-
-class DFAdapter(OutputAdapter):
-    def transform_data(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df
