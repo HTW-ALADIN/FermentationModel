cb4c4b955f3afb9ec676c65c39429e6da8d52517
Paul Christ <paul.l.christ@web.de>
2025-12-19 05:45:54 +0100

    fix: convert to static property

--- src/Util/strategy_pattern.py
+++ src/Util/strategy_pattern.py
@@ -7,9 +7,9 @@ class Strategy(Generic[T]):
     strategies: Dict[str, T]
 
     @staticmethod
-    def register_strategy(self, strategy: T):
-        self.strategies[type(strategy).__name__]
+    def register_strategy(strategy: T):
+        Strategy.strategies[type(strategy).__name__]
 
     @staticmethod
-    def select_strategy(self, strategy_name: str):
-        return self.strategies[strategy_name]()
+    def select_strategy(strategy_name: str):
+        return Strategy.strategies[strategy_name]()
