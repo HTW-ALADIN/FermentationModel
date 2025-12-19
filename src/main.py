0146a8a94a91301f89cba1550ad7313ca464d056
Paul Christ <paul.l.christ@web.de>
2025-12-19 06:12:55 +0100

    refactor: add parser and strategies to main

--- src/main.py
+++ src/main.py
@@ -5,9 +5,10 @@ import pandas as pd
 from calc_bioreactor import calculate
 from multiplot_ferm import multiplot_ferm
 from Util.base_io_adapter import IOAdapter
-from OutputAdapter.chart_js_adapter2 import ChartJSAdapter
-from InputAdapter.file_input_adapter import FileInputAdapter
+from OutputAdapter.output_adapter_strategy import output_adapter_strategy
+from InputAdapter.input_adapter_strategy import input_adapter_strategy
 from typing import Tuple
+from Util.arg_parser import parser
 
 
 def main() -> Tuple[pd.DataFrame, IOAdapter]:
@@ -15,21 +16,17 @@ def main() -> Tuple[pd.DataFrame, IOAdapter]:
     TODO: Add selection logic (cli-parameters) to select InputAdapter if required.
     Default is STDINAdapter
     """
-    
-    input_adapter = FileInputAdapter()
+    args = parser.parse_args()
+    print(args)
 
-    if len(sys.argv) < 2:
-        print('Usage: python script.py \'{"key": "value"}\'')
-        sys.exit(1)
+    input_adapter = input_adapter_strategy.select_strategy(args.input_type)
 
-    arg = sys.argv[1]
-    input_df = input_adapter.transform_data(arg)
-    #print(input_df)
+    input_df = input_adapter.transform_data(args.parameters)
     """
     TODO: Selection logic (cli-parameters) to select OutputAdapter if required.
     Default is ChartJSAdapter
     """
-    output_adapter = ChartJSAdapter()
+    output_adapter = output_adapter_strategy.select_strategy(args.output_type)
 
     return input_df, output_adapter
 
@@ -39,9 +36,9 @@ if __name__ == "__main__":
     result_df = calculate(input_df)
 
     output = output_adapter.transform_data(result_df)
-    #print(output)
-    #sys.stdout.write(json.dumps(output))
+    # print(output)
+    # sys.stdout.write(json.dumps(output))
     with open("output.json", "w") as f:
         json.dump(output, f)
-    multiplot_ferm(result_df)    
+    multiplot_ferm(result_df)
     sys.exit(0)
